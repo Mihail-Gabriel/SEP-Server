5fee2dbe96be4fa1109952499eb01b22299bfea7
FlaviusAB <31958193+FlaviusAB@users.noreply.github.com>
2021-05-17 17:48:53 +0200

    register user done

---
+++ Sep3/HttpServices/UserWebService.cs
@@ -0,0 +1,42 @@
+﻿using System.Net.Http;
+using System.Net.Http.Json;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+using Sep3.Models;
+
+namespace Sep3.HttpServices
+{
+    public class UserWebService : IUserService
+    {
+        private readonly HttpClient _client;
+
+        public UserWebService(HttpClient client)
+        {
+            this._client = client;
+        }
+
+        public async Task<User> ValidateUserAsync(string username, string password)
+        {
+            
+            var url = "http://localhost:8080/user/login?username=" + username + "&password=" + password;
+            var resp =  await   _client.GetAsync(url);
+            var user = resp.Content.ReadFromJsonAsync<User>().Result;
+            return user;
+        
+        }
+
+        public async Task<User> RegisterUserAsync(string username, string password, string number, string address, string city)
+        {
+            var url = "http://localhost:8080/user/register";
+            
+            User newUser = new User {username = username, password = password, telephoneNo = number, address = address, city = city, securityLevel = "0"};
+            string userAsJson = JsonSerializer.Serialize(newUser);
+            HttpContent content = new StringContent(userAsJson, Encoding.UTF8, "application/json");
+            
+            var resp =  await _client.PostAsync(url,content);
+            var user = resp.Content.ReadFromJsonAsync<User>().Result;
+            return user;
+        }
+    }
+}
---
+++ Sep3/Models/User.cs
@@ -0,0 +1,12 @@
+﻿namespace Sep3.Models
+{
+    public class User
+    {
+        public string username { get; set; }
+        public string password { get; set; }
+        public string address { get; set; }
+        public string telephoneNo { get; set; }
+        public string city { get; set; }
+        public string securityLevel { get; set; }
+    }
+}
