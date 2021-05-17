104a0fdcaad637b20fc605f0c31ebe0f49079101
FlaviusAB <31958193+FlaviusAB@users.noreply.github.com>
2021-05-17 13:12:02 +0200

    created branch

---
+++ Sep-Blazor-master/Sep3/HttpServices/IUserService.cs
@@ -0,0 +1,11 @@
+﻿using System.Threading.Tasks;
+using Sep3.Models;
+
+namespace Sep3.HttpServices
+{
+    public interface IUserService
+    {
+        Task<User> ValidateUserAsync(string username, string password);
+        Task<User> RegisterUserAsync(string username, string password, string number, string address, string city);
+    }
+}
---
+++ Sep-Blazor-master/Sep3/Models/User.cs
@@ -0,0 +1,12 @@
+﻿namespace Sep3.Models
+{
+    public class User
+    {
+        public string username { get; set; }
+        public string password { get; set; }
+        public string address { get; set; }
+        public string number { get; set; }
+        public string city { get; set; }
+        public string SecurityLevel { get; set; }
+    }
+}
