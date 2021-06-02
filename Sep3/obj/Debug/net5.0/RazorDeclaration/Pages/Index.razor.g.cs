ed8397e6c8b2b5e94c2833a1ea7c0bffa357f8fe
FlaviusAB <31958193+FlaviusAB@users.noreply.github.com>
2021-06-02 15:05:11 +0200

    working version

--- Sep3/obj/Debug/net5.0/RazorDeclaration/Pages/Index.razor.g.cs
+++ Sep3/obj/Debug/net5.0/RazorDeclaration/Pages/Index.razor.g.cs
@@ -13,70 +13,70 @@ namespace Sep3.Pages
     using System.Threading.Tasks;
     using Microsoft.AspNetCore.Components;
 #nullable restore
-#line 1 "C:\Users\Flavius-Alin\Desktop\Git OWN branches\Sep-Blazor\Sep3\_Imports.razor"
+#line 1 "C:\Users\Flavius-Alin\Downloads\Sep-Blazor-caa4fc810de67eb2b4432afdfb3ac2cbc884f647\Sep3\_Imports.razor"
 using System.Net.Http;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 2 "C:\Users\Flavius-Alin\Desktop\Git OWN branches\Sep-Blazor\Sep3\_Imports.razor"
+#line 2 "C:\Users\Flavius-Alin\Downloads\Sep-Blazor-caa4fc810de67eb2b4432afdfb3ac2cbc884f647\Sep3\_Imports.razor"
 using Microsoft.AspNetCore.Authorization;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 3 "C:\Users\Flavius-Alin\Desktop\Git OWN branches\Sep-Blazor\Sep3\_Imports.razor"
+#line 3 "C:\Users\Flavius-Alin\Downloads\Sep-Blazor-caa4fc810de67eb2b4432afdfb3ac2cbc884f647\Sep3\_Imports.razor"
 using Microsoft.AspNetCore.Components.Authorization;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 4 "C:\Users\Flavius-Alin\Desktop\Git OWN branches\Sep-Blazor\Sep3\_Imports.razor"
+#line 4 "C:\Users\Flavius-Alin\Downloads\Sep-Blazor-caa4fc810de67eb2b4432afdfb3ac2cbc884f647\Sep3\_Imports.razor"
 using Microsoft.AspNetCore.Components.Forms;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 5 "C:\Users\Flavius-Alin\Desktop\Git OWN branches\Sep-Blazor\Sep3\_Imports.razor"
+#line 5 "C:\Users\Flavius-Alin\Downloads\Sep-Blazor-caa4fc810de67eb2b4432afdfb3ac2cbc884f647\Sep3\_Imports.razor"
 using Microsoft.AspNetCore.Components.Routing;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 6 "C:\Users\Flavius-Alin\Desktop\Git OWN branches\Sep-Blazor\Sep3\_Imports.razor"
+#line 6 "C:\Users\Flavius-Alin\Downloads\Sep-Blazor-caa4fc810de67eb2b4432afdfb3ac2cbc884f647\Sep3\_Imports.razor"
 using Microsoft.AspNetCore.Components.Web;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 7 "C:\Users\Flavius-Alin\Desktop\Git OWN branches\Sep-Blazor\Sep3\_Imports.razor"
+#line 7 "C:\Users\Flavius-Alin\Downloads\Sep-Blazor-caa4fc810de67eb2b4432afdfb3ac2cbc884f647\Sep3\_Imports.razor"
 using Microsoft.AspNetCore.Components.Web.Virtualization;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 8 "C:\Users\Flavius-Alin\Desktop\Git OWN branches\Sep-Blazor\Sep3\_Imports.razor"
+#line 8 "C:\Users\Flavius-Alin\Downloads\Sep-Blazor-caa4fc810de67eb2b4432afdfb3ac2cbc884f647\Sep3\_Imports.razor"
 using Microsoft.JSInterop;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 9 "C:\Users\Flavius-Alin\Desktop\Git OWN branches\Sep-Blazor\Sep3\_Imports.razor"
+#line 9 "C:\Users\Flavius-Alin\Downloads\Sep-Blazor-caa4fc810de67eb2b4432afdfb3ac2cbc884f647\Sep3\_Imports.razor"
 using Sep3;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 10 "C:\Users\Flavius-Alin\Desktop\Git OWN branches\Sep-Blazor\Sep3\_Imports.razor"
+#line 10 "C:\Users\Flavius-Alin\Downloads\Sep-Blazor-caa4fc810de67eb2b4432afdfb3ac2cbc884f647\Sep3\_Imports.razor"
 using Sep3.Shared;
 
 #line default
